af8e776c4bdc3d68338dddf1fd3c96d57f53054f
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-10-25 20:24:41 +0200

    Initial commit

---
+++ Cinema Room Manager/task/src/Cinema.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Cinema {
+
+    public static void main(String[] args) {
+        final var scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of rows:");
+        final var rows = scanner.nextInt();
+
+        System.out.println("Enter the number of seats in each row:");
+        final var cols = scanner.nextInt();
+
+        final var seats = rows * cols;
+
+        final var income = seats > 60
+                ? 8 * seats + 2 * (rows / 2 * cols)
+                : 10 * seats;
+
+        System.out.println("Total income:");
+        System.out.println("$" + income);
+    }
+}
